9d0d6ad346ea4be1999fe9a28ac3db79322fb56b
Michael Tulmen <michaeltulmen@gmail.com>
2022-06-11 15:18:37 -0500

    added auth and helpers from Proj 2

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
-const express = require('express');
-const sequelize = require('sequelize');
-const path = require('path');
+//imported from project
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("sequelize");
+const helpers = require("./utils/helpers");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const sess = {
+  secret: "its a secret duh",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(routes);
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`Running on port ${PORT}!`));
+});
---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+//imported from Proj2
+
+function formatDate(date) {
+  return `${new Date(date).getMonth() + 1}/${new Date(
+    date
+  ).getDate()}/${new Date(date).getFullYear()}`;
+}
+
+module.exports = {
+  formatDate,
+};
